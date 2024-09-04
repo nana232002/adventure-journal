a4c50dd62c6d133fa2fc01bb6bd91d51d2cbb1e7
nana232002 <nanaomar457@gmail.com>
2024-09-04 16:18:17 +0300

    done

---
+++ 4.9/javaScript.js
@@ -0,0 +1,80 @@
+// exersise 1
+
+// This is a single-line comment
+/* 
+This is a multi-line comment.
+It can span multiple lines.
+*/
+// exersise 2
+console.log("exersise 2");
+const myName = "Nana";
+console.log(myName);
+const num1 = 5;
+const num2 = 25;
+console.log(num1+num2);
+const flage = true;
+console.log(flage);
+let value1 = undefined;
+console.log(value1);
+let value2 = null;
+// exersise 3
+console.log("exersise 3");
+const parisTempreture = 21;
+//console.log(" the tempreture of paris is:" ,tempreture);
+const IsExist =false;
+const EmailADress = "nana@gmail.com";
+const BaskeBallScore = 25;
+
+// exersise 4
+console.log("exersise 4");
+var num =25;
+let num3 ="strig";
+const boolValue =false;
+console.log(num);
+num = 0;
+console.log(num);
+
+console.log(num3);
+num3 = "done";
+console.log(num3);
+
+// exersise 5
+const Name = "Nana";
+const age = 22;
+console.log("my name is" ,Name ,"my age is", age);
+const lengh =5;
+const width =2;
+console.log("the area is" , lengh*width , "with width", width, "and lengh",lengh);
+const day="friday";
+const task1="sleeping";
+const task2 ="weekend exercise";
+const task3 ="summerizing";
+console.log("In" , day , "I have three task  : ", task1, task2,task3);
+const petNum =3;
+console.log("I have " +petNum +" pet");
+const triangleArea = (lengh*width)/2;
+console.log("the triangle area is " ,triangleArea );
+ 
+// exersise 5
+console.log(typeof num);
+console.log(typeof value1);
+console.log(typeof value2);
+// exersice 8 //skip 1-6 done 7-9 
+if (num%2==0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+if (num %7==0 && num %5 ==0){
+    console.log("ok");
+}
+else{
+    console.log("not ok");
+}
+
+for(let nu=0; nu<=20;nu++){
+    if (num %4==0 ){
+        console.log(nu);
+    }
+}
